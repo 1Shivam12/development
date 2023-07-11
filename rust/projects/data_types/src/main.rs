fa842b869f7b83b6e751675b8503fba770440d4f
Shivam Bhatnagar <ShivamBhatnagar@MacBook-Air.local>
2023-07-11 09:35:19 +0100

    feat: carry on data types

--- rust/projects/data_types/src/main.rs
+++ rust/projects/data_types/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
     // Type Annotation for Ambiguous Variables
+    // without u32 here it throws an error as parse can't determine what to convert to
     let guess: u32 = "42".parse().expect("Not a number");
-    println!("{guess}")
+    println!("{guess}");
+
+    // this works fine as i32 (aka signed), u32 causes a panic as unsigned can't be negative
+    let num: i32 = "-42".parse().expect("Not a number");
+    println!("{num}")
+
 }
